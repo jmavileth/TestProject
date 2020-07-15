35e880efae49f1eed03c00fd73db1e7ccad03c46
John-PC <John-PC@DESKTOP-C30CHQT>
2020-07-15 22:19:10 +0530

    First Commit

---
+++ src/dingi/Dinga.java
@@ -0,0 +1,9 @@
+package dingi;
+
+public class Dinga {
+
+	public static void main(String[] args) {
+		System.out.println("Pengi");
+	}
+
+}
---
+++ src/dingi/Mengi.java
@@ -0,0 +1,10 @@
+package dingi;
+
+public class Mengi {
+
+	public static void main(String[] args) {
+		System.out.println("Menga");
+
+	}
+
+}
